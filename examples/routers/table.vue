c5b1d2b79c864ef5d6d1b75af0c0cc6630bfe110
yyg <https://gitee.com/yangyg10>
2020-08-26 17:49:47 +0800

    update: table example

--- examples/routers/table.vue
+++ examples/routers/table.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="margin: 200px">
+    <div style="margin: 50px">
         <Divider>普通表格</Divider>
         <p>
             size 
@@ -43,7 +43,6 @@
 
         <Divider>Select</Divider>
         <p>
-            手风琴 accordion
             <Button size="small" @click="handleSelectAll(true)">设置全选</Button>
             <Button size="small" @click="handleSelectAll(false)">取消全选</Button>
         </p>
@@ -53,6 +52,28 @@
         <div style="margin: 5px 0 20px;">
             <Table ref="selectTable2" size="small" noBorder stripe highlight-row row-key="id" :columns="columns5" :data="data2"></Table>
         </div>
+
+        <Divider>Column Drag</Divider>
+        <p>type列(type存在，不为空) 不可拖动</p>
+        <div style="margin: 5px 0 20px;">
+            <Table
+                size="small" noBorder stripe highlight-row draggable column-draggable
+                :columns="columns2"
+                :data="data1"
+                @on-drag-drop="odd"
+                @on-column-drag-drop="(i, j) => ocdd(i, j, columns2)"
+            />
+        </div>
+        <p>fixed列、表头分组(子表头) 不可拖动</p>
+        <div style="margin: 5px 0 20px;">
+            <Table
+                size="small" noBorder stripe highlight-row draggable column-draggable
+                :columns="columns6"
+                :data="data1"
+                @on-drag-drop="odd"
+                @on-column-drag-drop="(i, j) => ocdd(i, j, columns6)"
+            />
+        </div>
     </div>
 </template>
 <script>
@@ -61,6 +82,26 @@
         components: { expandRow },
         data () {
             return {
+                columns: [
+                    {
+                        title: 'Date',
+                        key: 'date',
+                        width: 180,
+                    },
+                    {
+                        title: 'Name',
+                        key: 'name',
+                        tree: true
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
                 columns1: [
                     {
                         title: 'Date',
@@ -238,31 +279,95 @@
                         key: 'address'
                     }
                 ],
-                data1: [
+                columns6: [
                     {
-                        name: 'John Brown',
-                        age: 18,
-                        address: 'New York No. 1 Lake Park',
-                        date: '2016-10-03',
+                        title: 'Name',
+                        key: 'name',
+                        align: 'center',
+                        width: 200,
+                        fixed: 'left',
                     },
                     {
-                        name: 'Jim Green',
-                        age: 24,
-                        address: 'London No. 1 Lake Park',
+                        title: 'Other',
+                        align: 'center',
+                        children: [
+                            {
+                                title: 'Age',
+                                key: 'age',
+                                align: 'center',
+                                width: 200,
+                            },
+                            {
+                                title: 'Address',
+                                key: 'address',
+                                align: 'center',
+                                width: 200,
+                            }
+                        ]
+                    },
+                    {
+                        title: 'Company',
+                        align: 'center',
+                        children: [
+                            {
+                                title: 'Company Address',
+                                key: 'caddress',
+                                align: 'center',
+                                width: 200
+                            },
+                            {
+                                title: 'Company Name',
+                                key: 'cname',
+                                align: 'center',
+                                width: 200
+                            }
+                        ]
+                    },
+                    {
+                        title: 'Gender',
+                        key: 'gender',
+                        align: 'center',
+                        width: 200,
+                        fixed: 'right'
+                    }
+                ],
+                data1: [
+                    {
+                        name: 'John Brown 1',
+                        age: 1,
+                        address: 'Lake Park 1',
+                        caddress: 'Lake Street 1',
+                        cname: 'SoftLake Co 1',
+                        gender: 'M 1',
                         date: '2016-10-01',
                     },
                     {
-                        name: 'Joe Black',
-                        age: 30,
-                        address: 'Sydney No. 1 Lake Park',
-                        date: '2016-10-02'
+                        name: 'John Brown 2',
+                        age: 2,
+                        address: 'Lake Park 2',
+                        caddress: 'Lake Street 2',
+                        cname: 'SoftLake Co 2',
+                        gender: 'M 2',
+                        date: '2016-10-02',
                     },
                     {
-                        name: 'Jon Snow',
-                        age: 26,
-                        address: 'Ottawa No. 2 Lake Park',
-                        date: '2016-10-04'
-                    }
+                        name: 'John Brown 3',
+                        age: 3,
+                        address: 'Lake Park 3',
+                        caddress: 'Lake Street 3',
+                        cname: 'SoftLake Co 3',
+                        gender: 'M 3',
+                        date: '2016-10-03',
+                    },
+                    {
+                        name: 'John Brown 4',
+                        age: 4,
+                        address: 'Lake Park 4',
+                        caddress: 'Lake Street 4',
+                        cname: 'SoftLake Co 4',
+                        gender: 'M 4',
+                        date: '2016-10-04',
+                    },
                 ],
                 data2: [
                     {
@@ -327,8 +432,12 @@
         },
         methods: {
             odd (index1, index2) {
-                console.log(index1, index2)
-                // this.data1.splice(index2, 0, this.data1.splice(index1, 1)[0])
+                console.log('odd', index1, index2);
+                this.data1.splice(index2, 0, this.data1.splice(index1, 1)[0]);
+            },
+            ocdd (index1, index2, columns) {
+                console.log('ocdd', index1, index2);
+                columns.splice(index2, 0, columns.splice(index1, 1)[0]);
             },
             handleSelectAll (status) {
                 this.$refs.selectTable.selectAll(status);
